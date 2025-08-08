8d429df8a9e140afa8b47ce6f8d5ecd262e73f2d
AshishG108 <123228006+AshishG108@users.noreply.github.com>
2025-08-08 22:22:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("welcome to spotify");
+
+//Initialize the Variables
+let songIndex=0;
+let audioElement=new Audio("songs/1.mp3");
+let masterPlay=document.getElementById("masterPlay");
+let myProgressBar=document.getElementById("myProgressBar");
+let gif=document.getElementById("gif");
+let songs=[
+    {songName:"Salam-e-Ishq",filePath:"./songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/2.mp3",coverPath:"covers/2.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/3.mp3",coverPath:"covers/3.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/4.mp3",coverPath:"covers/4.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/5.mp3",coverPath:"covers/5.jpg"}, 
+    {songName:"Salam-e-Ishq",filePath:"./songs/6.mp3",coverPath:"covers/6.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/7.mp3",coverPath:"covers/7.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/8.mp3",coverPath:"covers/8.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"./songs/9.mp3",coverPath:"covers/9.jpg"},
+]
+
+
+//audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+       audioElement.play(); 
+       masterPlay.classList.remove('fa-solid fa-play');
+       masterPlay.classList.add('fa-solid fa-pause');
+       gif.style.opacity=1;
+    }
+
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause');
+        masterPlay.classList.add('fa-play');
+        gif.style.opacity=0;
+    }
+})
+//Listen to Events
+audioElement.addEventListner('timeupdate',() => {
+//update Seekbar
+let progress=parseInt((audioElement.currentTime/audioElement.duration)* 100);
+myProgressBar.value=progress;
+})
+
+myProgressBar.addEventListner('change', () =>{
+    audioElement.currentTime=myProgressBar.value* audioElement.duration/100;
+})
+
+
